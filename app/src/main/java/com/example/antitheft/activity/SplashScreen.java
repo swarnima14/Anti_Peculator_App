0db2d816b46ebf3ff843f9a43f757e0b50fd1be2
swarnima14 <swarnima.tiwari14@gmail.com>
2020-08-05 16:38:00 +0530

    splash screen added

---
+++ app/src/main/java/com/example/antitheft/activity/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.example.antitheft.activity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import gr.net.maroulis.library.EasySplashScreen;
+
+import com.example.antitheft.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EasySplashScreen config=new EasySplashScreen(SplashScreen.this)
+                .withFullScreen()
+                .withTargetActivity(WelcomeActivity.class)
+                .withSplashTimeOut(1500)
+                .withBackgroundColor(Color.parseColor("#000000"))
+                .withLogo(R.drawable.mechanic);
+
+        View easySplashScreen=config.create();
+        setContentView(easySplashScreen);
+    }
+}
+
